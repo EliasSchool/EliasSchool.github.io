cca5f2678444bd1594195d35823be937b503f71d
EliasSchool <elias.talonen@edu.keuda.fi>
2023-03-08 09:31:44 +0200

    lisätty register.js

---
+++ pizzeria/js/register.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut)
+
+// register nappulan functio 
+function onregisterClick() {
+    //haetaan evästeistä nimi ja salasana
+    const nimi = document.getElementById("nimi").value
+    const salasana = document.getElementById("salasana").value
+    //katsotaan että nimi ja salasana ei ole tyhjä stringejä 
+    if(nimi && salasana) {
+        //laitetaan nimi ja salasana evästeisiin joka tulee olemaan inputti molemmista
+        localStorage.setItem("nimi", nimi)
+        localStorage.setItem("salasana", salasana)
+    } else {
+        console.log("joo")
+    }
+}
