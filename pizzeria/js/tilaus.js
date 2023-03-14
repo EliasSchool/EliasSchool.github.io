ce69bc2e8659e95fedd752d3f8d7d5ed567e1605
EliasSchool <elias.talonen@edu.keuda.fi>
2023-03-14 13:13:54 +0200

    lisätty tilaus ja ostoskori hommat

--- pizzeria/js/tilaus.js
+++ pizzeria/js/tilaus.js
@@ -1,21 +1,32 @@
 const addToCartButton = document.getElementById('add-to-cart-button');
 const menuItemCheckboxes = document.querySelectorAll('.menu-item-checkbox');
-
-let cartItems = [];
+const cartItemsDropdown = document.getElementById('cart-items');
+const notification = document.getElementById("notification")
 
 addToCartButton.addEventListener('click', () => {
-  cartItems = [];
-  localStorage.setItem("ostoskori",JSON.stringify(cartItems))
-
+  const cartItems = [];
   menuItemCheckboxes.forEach((checkbox) => {
     if (checkbox.checked) {
       const menuItem = checkbox.closest('.row').querySelector('h4').innerText;
       const menuPrice = checkbox.closest('.row').querySelector('.menu-price').innerText;
       cartItems.push({ name: menuItem, price: menuPrice });
+      checkbox.checked = false
     }
   });
-  let ostoskori = JSON.stringify(cartItems)
-  localStorage.setItem("ostoskori", ostoskori)
-  const joo = localStorage.getItem("ostoskori")
-  console.log(joo)
+  console.log(cartItems)
+  localStorage.setItem("ostoskori", JSON.stringify(cartItems));
+
+  // Display a notification that items have been added to the cart
+  if (cartItems.length > 0) {
+    if(cartItems.length === 1) {
+        notification.textContent = `${cartItems.length} tuote lisätty ostoskoriin`;
+    } else {
+        notification.textContent = `${cartItems.length} tuotetta lisätty ostoskoriin`;
+    }
+    notification.style.display = 'block';
+    // Hide the notification after 3 seconds
+    setTimeout(() => {
+      notification.style.display = 'none';
+    }, 3000);
+  }
 });
