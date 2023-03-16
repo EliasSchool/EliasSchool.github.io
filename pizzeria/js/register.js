7bf3ad18e6963e848ef3eda4982c8a1343f369c1
octc09 <oliveracaldwell@gmail.com>
2023-03-16 20:19:02 +0200

    Kirjautuminen tehty
    käyttäjä nimi näkyy kaikkilla sivuilla

--- pizzeria/js/register.js
+++ pizzeria/js/register.js
@@ -5,13 +5,26 @@ function onregisterClick() {
     //haetaan evästeistä nimi ja salasana
     const nimi = document.getElementById("nimi").value
     const salasana = document.getElementById("salasana").value
+    
     //katsotaan että nimi ja salasana ei ole tyhjä stringejä 
-    if(nimi && salasana) {
-        //laitetaan nimi ja salasana evästeisiin joka tulee olemaan inputti molemmista
-        localStorage.setItem("nimi", nimi)
-        localStorage.setItem("salasana", salasana)
-        window.location.replace("index.html")
-    } else {
-        console.log("joo")
+    if(nimi && salasana) 
+    {
+      //laitetaan nimi ja salasana evästeisiin joka tulee olemaan inputti molemmista
+      localStorage.setItem("nimi", nimi)
+      localStorage.setItem("salasana", salasana)
+      //Otetaan muistosta nimi
+      const name = localStorage.getItem("nimi");
+      //näytetään nimi
+      const nameDisplay = document.getElementById("name-display");
+      nameDisplay.textContent = name;
+      //viedään etusivulle
+      window.location.href = "index.html"
     }
 }
+//tämä osa activoituu sivuston laukausussa
+document.addEventListener("DOMContentLoaded", function launch() {
+  const name = localStorage.getItem("nimi");
+  const nameDisplay = document.getElementById("name-display");
+  nameDisplay.textContent = name;
+});
+
